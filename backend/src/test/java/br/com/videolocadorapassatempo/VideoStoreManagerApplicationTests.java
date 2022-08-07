08d999f4aa51004835dfeab86c18edae83b5c6a8
Mateus Padovan Pereira <mateuspadovanpereira@hotmail.com>
2022-08-07 19:45:44 -0300

    Dependências configuradas

---
+++ backend/src/test/java/br/com/videolocadorapassatempo/VideoStoreManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.videolocadorapassatempo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoStoreManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
