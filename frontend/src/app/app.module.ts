a4dcb38a194e2cea8612eefa511bc8eda75496fd
Álisson Melquíades <armelquiades2000@gmail.com>
2022-08-13 17:31:49 -0300

    feature/footer

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,18 +4,33 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 
-import { NzCardModule } from 'ng-zorro-antd/card';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { pt_BR } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import pt from '@angular/common/locales/pt';
+
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzTypographyModule } from 'ng-zorro-antd/typography';
+
+registerLocaleData(pt);
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     CoreModule,
     AppRoutingModule,
-    NzCardModule
+    NzLayoutModule,
+    NzPageHeaderModule,
+    NzMenuModule,
+    NzIconModule,
+    NzBreadCrumbModule,
+    NzTypographyModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [{ provide: NZ_I18N, useValue: pt_BR }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { HttpClientModule } from '@angular/common/http';
+import { FooterComponent } from './footer/footer.component';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @NgModule({
   imports: [
@@ -11,8 +14,19 @@ import { HttpClientModule } from '@angular/common/http';
     HttpClientModule,
     BrowserModule,
     BrowserAnimationsModule,
+    NzLayoutModule,
+    NzMenuModule,
+    NzIconModule
   ],
-  declarations: [],
-  exports: [HttpClientModule, BrowserModule, BrowserAnimationsModule],
+  declarations: [
+    FooterComponent,
+  ],
+  exports: [
+    HttpClientModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+
+    FooterComponent,
+  ],
 })
 export class CoreModule {}
