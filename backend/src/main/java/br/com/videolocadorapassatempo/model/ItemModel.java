dc6c3cfb44f0143265bb2d19a127fdd5962bf293
Mateus Padovan Pereira <mateuspadovanpereira@hotmail.com>
2022-11-15 14:51:09 -0300

    Corrigido mapeamento da entidade Item

--- backend/src/main/java/br/com/videolocadorapassatempo/model/ItemModel.java
+++ backend/src/main/java/br/com/videolocadorapassatempo/model/ItemModel.java
@@ -18,15 +18,15 @@ public class ItemModel implements Serializable {
     @SequenceGenerator(name = "sequence_item", sequenceName = "sequence_item", allocationSize = 1)
     private Long id;
 
-    @JoinColumn(name = "serial_number", nullable = false)
+    @Column(name = "serial_number", nullable = false)
     private String serialNumber;
+
+    @Column(name = "purchase_date", nullable = false)
+    private LocalDate purchaseDate;
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "id_title", referencedColumnName = "id", nullable = false)
     private TitleModel titleModel;
-
-    @JoinColumn(name = "purchase_date", nullable = false)
-    private LocalDate purchaseDate;
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "id_item_type", nullable = false)
