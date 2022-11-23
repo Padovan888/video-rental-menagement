5ded7d4f2efa96f30700e0d000edf336f73c1059
Mateus Padovan Pereira <mateuspadovanpereira@hotmail.com>
2022-11-22 22:59:37 -0300

    Refatoração da classe LocationServiceImpl

--- backend/src/main/java/br/com/videolocadorapassatempo/service/LocationService.java
+++ backend/src/main/java/br/com/videolocadorapassatempo/service/LocationService.java
@@ -1,25 +1,21 @@
-//package br.com.videolocadorapassatempo.service;
-//
-//import br.com.videolocadorapassatempo.service.dto.LocationDto;
-//
-//import java.util.List;
-//
-//public interface LocationService {
-//
-//    List<LocationDto> findAll();
-//
-//    LocationDto findById(Long idLocation);
-//
-//    LocationDto create(LocationDto locationDto);
-//
-//    LocationDto update(LocationDto locationDto);
-//
-//    void deleteById(Long idLocation);
-//
-//    LocationDto customerDebitRecords(Long idCustomer);
-//
-//    LocationDto dependentDebitRecords(Long idDependent);
-//
-//    LocationDto makeReturn(String serialNumber);
-//
-//}
+package br.com.videolocadorapassatempo.service;
+
+import br.com.videolocadorapassatempo.service.dto.LocationDto;
+
+import java.util.List;
+
+public interface LocationService {
+
+    List<LocationDto> findAll();
+
+    LocationDto findById(Long idLocation);
+
+    LocationDto create(LocationDto locationDto);
+
+    LocationDto update(LocationDto locationDto);
+
+    void deleteById(Long idLocation);
+
+    LocationDto makeReturn(String serialNumber);
+
+}
--- backend/src/main/java/br/com/videolocadorapassatempo/service/mapper/LocationMapper.java
+++ backend/src/main/java/br/com/videolocadorapassatempo/service/mapper/LocationMapper.java
@@ -1,23 +1,21 @@
-//package br.com.videolocadorapassatempo.service.mapper;
-//
-//import br.com.videolocadorapassatempo.model.LocationModel;
-//import br.com.videolocadorapassatempo.service.dto.LocationDto;
-//import org.mapstruct.Mapper;
-//import org.mapstruct.Mapping;
-//
-//@Mapper(componentModel = "spring")
-//public interface LocationMapper extends EntityMapper<LocationDto, LocationModel> {
-//
-//    @Override
-//    @Mapping(source = "customerModel.id", target = "idCustomer")
-//    @Mapping(source = "dependentModel.id", target = "idDependent")
-//    @Mapping(source = "itemModel.id", target = "idItem")
-//    LocationDto toDto(LocationModel entity);
-//
-//    @Override
-//    @Mapping(source = "idCustomer", target = "customerModel.id")
-//    @Mapping(source = "idDependent", target = "dependentModel.id")
-//    @Mapping(source = "idItem", target = "itemModel.id")
-//    LocationModel toEntity(LocationDto dto);
-//
-//}
+package br.com.videolocadorapassatempo.service.mapper;
+
+import br.com.videolocadorapassatempo.model.LocationModel;
+import br.com.videolocadorapassatempo.service.dto.LocationDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface LocationMapper extends EntityMapper<LocationDto, LocationModel> {
+
+    @Override
+    @Mapping(source = "customerModel.id", target = "idCustomer")
+    @Mapping(source = "itemModel.id", target = "idItem")
+    LocationDto toDto(LocationModel entity);
+
+    @Override
+    @Mapping(source = "idCustomer", target = "customerModel.id")
+    @Mapping(source = "idItem", target = "itemModel.id")
+    LocationModel toEntity(LocationDto dto);
+
+}
