972a0048585942d98e053e28059115d838b60a01
Álisson Melquíades <armelquiades2000@gmail.com>
2022-11-23 10:53:33 -0300

    Feature: List all members

---
+++ backend/src/main/java/br/com/videolocadorapassatempo/service/dto/ViewMemberDto.java
@@ -0,0 +1,39 @@
+package br.com.videolocadorapassatempo.service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ViewMemberDto implements Serializable {
+
+    private Long id;
+
+    private Long registrationNumber;
+
+    private String name;
+
+    private LocalDate birthDate;
+
+    private Character gender;
+    
+    private String cpf;
+    
+    private String address;
+    
+    private String telephone;
+
+    private Boolean active;
+
+    private List<CustomerDto> dependents = new ArrayList<>();
+
+}
